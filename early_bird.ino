0c64442fbaa26d4a0477b4aa01049fbe499804e2
juicyjung <travelandi01@gmail.com>
2021-11-18 12:36:03 +0900

    gui try

--- early_bird.ino
+++ early_bird.ino
@@ -1,3 +1,6 @@
+#include <Time.h>
+#include <TimeLib.h>
+
 #define BUZZER 8
 #define BUTTON1 6
 #define BUTTON2 7
@@ -13,32 +16,34 @@
 
 int notes[25] = { G, G, A, A, G, G, E, G, G, E, E, D, G, G, A, A, G, G, E, G, E, D, E, C };
 int num = 0;
+
 void setup(){
   Serial.begin(9600);
   pinMode(RELAY,OUTPUT); 
   pinMode(BUZZER,OUTPUT);
   pinMode(BUTTON1,INPUT);
   pinMode(BUTTON2,INPUT); 
+
+  setTime(16,0,0,17,11,21);
 } 
 
 int i =0;
 int count1 =0;
 int count2 =0;
 
-void loop(){ 
-  digitalWrite(RELAY,HIGH); 
-  while(count2<3){
-    tone (BUZZER, notes[ i ], 200);
-    delay(800);
-    i++;
-    if(i == 12)
-      delay(200);
-    if(i==24)
-      i=0;
-      delay(200);
+void ringing(){
+  tone (BUZZER, notes[ i ], 200);
+  delay(800);
+  i++;
+  if(i == 12)
+   delay(200);
+  if(i==24)
+   i=0;
+   delay(200);
+}
 
-      
-    if(digitalRead(BUTTON1)==HIGH){
+void turn_off(){
+  if(digitalRead(BUTTON1)==HIGH){
       count1 = 1;
       switch(count2){
         case(1): count1 =2; break;
@@ -52,6 +57,44 @@ void loop(){
          case(3): count2 =3;
        }
     }
+}
+
+void digitalClockDisplay()
+{
+  Serial.print(hour());
+  printDigits(minute());
+  printDigits(second());
+  Serial.print(" ");
+  Serial.print(day());
+  Serial.print(" ");
+  Serial.print(month());
+  Serial.print(" ");
+  Serial.print(year());
+  Serial.println();
+}
+
+void printDigits(int digits)
+{
+  // utility function for clock display: prints preceding colon and leading 0
+  Serial.print(":");
+  if(digits < 10)
+    Serial.print('0');
+  Serial.print(digits);  
+}
+
+void loop(){
+  
+  while(Serial.available()){
+    char data = Serial.read();
+    Serial.print(data);
+  }
+  
+  digitalWrite(RELAY,HIGH); 
+  while(count2<3){
+    ringing();
+
+    turn_off();
+    
     Serial.println(count1);
     Serial.println(count2);
   }
